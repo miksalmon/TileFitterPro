5bdd605d75c4095b930119ad617a59efd0629503
Michael Salmon <miksalmon@users.noreply.github.com>
2022-02-10 18:17:20 -0800

    add uwp sample app

---
+++ TileFitterPro/Behaviors/NavigationViewHeaderMode.cs
@@ -0,0 +1,9 @@
+﻿namespace TileFitterPro.Behaviors
+{
+    public enum NavigationViewHeaderMode
+    {
+        Always,
+        Never,
+        Minimal
+    }
+}
---
+++ TileFitterPro/Views/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using TileFitterPro.ViewModels;
+
+using Windows.UI.Xaml.Controls;
+
+namespace TileFitterPro.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainViewModel ViewModel { get; } = new MainViewModel();
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
